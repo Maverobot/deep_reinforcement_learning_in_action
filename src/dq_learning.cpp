05197d9c344b085f9e2063bda3bdfed48334d784
Zheng Qu <quzhengrobot@gmail.com>
2020-04-10 16:16:15 +0200

    added test model

--- src/dq_learning.cpp
+++ src/dq_learning.cpp
@@ -8,6 +8,8 @@
 #include <random>
 #include <vector>
 
+using drl_in_action::grid_world::GridWorld;
+
 template <class T>
 std::ostream& operator<<(std::ostream& o, const std::vector<T>& arr) {
   copy(arr.cbegin(), arr.cend(), std::ostream_iterator<T>(o, " "));
@@ -42,16 +44,61 @@ torch::Tensor flat_tensor(std::vector<std::vector<value_type>> input,
   return torch::cat(output_vec, 1);
 }
 
-using drl_in_action::grid_world::GridWorld;
-
 constexpr int kMaxSteps = 50;
 constexpr float kLearningRate = 0.001f;
 constexpr float kGamma = 0.9f;
 constexpr int kEpochs = 1000;
+const std::string kModelFile = "dq_model.pt";
+
+void test_model(torch::nn::Sequential& model) {
+  // RNG
+  std::random_device rd;
+  std::default_random_engine generator(rd());
+  std::uniform_real_distribution<double> dist_real(0.0, 1.0);
+  const auto explore_rand = [&generator, &dist_real] { return dist_real(generator); };
+  std::uniform_int_distribution<int> dist_int(0, magic_enum::enum_count<GridWorld::Action>() - 1);
+  const auto action_rand = [&generator, &dist_int] { return dist_int(generator); };
+
+  auto game = drl_in_action::grid_world::GridWorld();
+  game.display();
+  int step_cout = 0;
+  float epsilon = 0.0f;
+  auto state = flat_tensor(game.state());
+  while (step_cout++ < kMaxSteps && !game.over()) {
+    // Get Q values for the current state
+    auto q_values = model->forward(state);
+
+    // Either explore with random action or pick the one with the highest Q value
+    int action = 0;
+    if (explore_rand() < epsilon) {
+      action = action_rand();
+    } else {
+      action = q_values.argmax().item<int>();
+    }
+    auto [reward, new_state_raw] = game.step(GridWorld::Action(action), true);
+    state = flat_tensor(new_state_raw);
+    game.display();
+  }
+  std::cout << "step used: " << step_cout - 1 << "\n";
+}
 
 int main(int argc, char* argv[]) {
   auto model = create_model(torch::kCUDA);
   auto optimizer = torch::optim::Adam(model->parameters(), kLearningRate);
+
+  bool model_loaded = true;
+  try {
+    torch::load(model, kModelFile);
+    std::cout << "model loaded\n";
+  } catch (...) {
+    model_loaded = false;
+    std::cout << "There is no model to load\n";
+  }
+
+  if (model_loaded) {
+    test_model(model);
+    return 0;
+  }
 
   // RNG
   std::random_device rd;
@@ -111,8 +158,11 @@ int main(int argc, char* argv[]) {
     if (epoch_idx > kEpochs / 2 && epsilon > 0.3f) {
       epsilon -= 1.0f / kEpochs;
     }
+
     printf("Epoch %lu total reward: %f, status: %s\n", epoch_idx, total_reward,
            game.win() ? "win" : "loss");
   }
+
+  torch::save(model, kModelFile);
   return 0;
 }
